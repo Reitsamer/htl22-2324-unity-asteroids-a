4589111b81965b9d216a2caf7bf2a5975769f627
Wolf Reitsamer <reit@htl-donaustadt.at>
2023-10-14 10:16:02 +0200

    Corrected RockCreator

--- Asteroids/Assets/Scripts/RockCreator.cs
+++ Asteroids/Assets/Scripts/RockCreator.cs
@@ -16,22 +16,24 @@ public class RockCreator : MonoBehaviour
 
     private void GenerateGeometry()
     {
-        lineRenderer.positionCount = 3;
-        lineRenderer.startWidth = 0.25f;
-        lineRenderer.endWidth = 0.25f;
-        lineRenderer.useWorldSpace = false;
-
-        Vector3[] positions = new Vector3[3];
-        // positions[0] = new Vector3(0, height * 0.5f, 0);
-        // positions[1] = new Vector3(-width * 0.5f, -height * 0.5f, 0);
-        // positions[2] = new Vector3(width * 0.5f, -height * 0.5f, 0);
-
-        lineRenderer.SetPositions(positions);
-        lineRenderer.loop = true;
-
-        // if (Application.isPlaying)
-        //     StartCoroutine(SetupEnginePosition(positions));
-        //
-        // spawnPoint = positions[0];
+        lr.positionCount=vertices;
+        lr.startWidth=0.25f;
+        lr.endWidth=0.25f;
+        //Quaternion rotate=Quaternion.Euler(0,0,360/Vertices);
+        Vector3[] positions=new Vector3[vertices];
+        positions[0]=initialPos;
+        for(int i=1;i<vertices;i++)
+        {
+            positions[i]=Quaternion.Euler(0,0,360*i/vertices)*initialPos;
+            float deformation = randomStartDeformation + Time.time * speed;
+            float offset = Mathf.PerlinNoise(
+                positions[i].x + deformation, 
+                positions[i].y + deformation ) * threshold;
+            float remappedOffset = math.remap(0f, 1f, -1f, 1f, offset);
+            positions[i].x += remappedOffset;
+            positions[i].y += remappedOffset;
+        }
+        lr.SetPositions(positions);
+        lr.loop=true;
     }
 }
