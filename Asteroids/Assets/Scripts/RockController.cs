30d3ef8566d584628673c1f093887dd5cbb7d763
Wolf Reitsamer <reit@htl-donaustadt.at>
2023-10-14 13:19:49 +0200

    Almost done (Day 1)

--- Asteroids/Assets/Scripts/RockController.cs
+++ Asteroids/Assets/Scripts/RockController.cs
@@ -1,18 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RockController : MonoBehaviour
 {
+    public Vector3 direction;
+    public float speed;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    private float leftScreenEdge;
+    private float rightScreenEdge;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftScreenEdge = Camera.main.ViewportToWorldPoint(
+            new Vector3(0, 0, 0)).x;
+        rightScreenEdge = Camera.main.ViewportToWorldPoint(
+            new Vector3(1, 0, 0)).x;
     }
 
+    private void RecalculateBoundingBox()
+    {
+        LineRenderer renderer = GetComponent<LineRenderer>();
+        Vector3[] positions = new Vector3[renderer.positionCount];
+        renderer.GetPositions(positions);
+
+        xMin = float.MaxValue;
+        xMax = float.MinValue;
+        yMin = float.MaxValue;
+        yMax = float.MinValue;
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            if (xMin > positions[i].x) 
+                xMin = positions[i].x;
+
+            // xMin = Math.Min(xMin, positions[i].x);
+            
+            xMax = Math.Max(xMax, positions[i].x);
+            yMin = Math.Min(yMin, positions[i].y);
+            yMax = Math.Max(yMax, positions[i].y);
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
+        transform.position += direction.normalized * Time.deltaTime * speed;
         
+        RecalculateBoundingBox();
+
+        if (direction.normalized.x < 0)
+        {
+            if (transform.position.x + (xMax - xMin) / 2 < leftScreenEdge)
+            {
+                transform.position = new Vector3(
+                    rightScreenEdge + (xMax - xMin) / 2,
+                    transform.position.y,
+                    transform.position.z);
+            }
+        }
+        else
+        {
+            
+        }
     }
 }
--- Asteroids/Assets/Scripts/RockCreator.cs
+++ Asteroids/Assets/Scripts/RockCreator.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class RockCreator : MonoBehaviour
 {
     public LineRenderer lineRenderer;
+    public int vertices;
+    public float lineWidth;
+    public Vector3 initialPos;
+    public float speed;
+
+    public float randomStartDeformation;
+    public float threshold;
     
     // Start is called before the first frame update
     void Start()
@@ -16,15 +26,16 @@ public class RockCreator : MonoBehaviour
 
     private void GenerateGeometry()
     {
-        lr.positionCount=vertices;
-        lr.startWidth=0.25f;
-        lr.endWidth=0.25f;
+        lineRenderer.positionCount=vertices;
+        lineRenderer.startWidth=lineWidth;
+        lineRenderer.endWidth=lineWidth;
         //Quaternion rotate=Quaternion.Euler(0,0,360/Vertices);
         Vector3[] positions=new Vector3[vertices];
         positions[0]=initialPos;
         for(int i=1;i<vertices;i++)
         {
-            positions[i]=Quaternion.Euler(0,0,360*i/vertices)*initialPos;
+            positions[i]=Quaternion.Euler(0,0,(360f*i)/vertices)*initialPos;
+            
             float deformation = randomStartDeformation + Time.time * speed;
             float offset = Mathf.PerlinNoise(
                 positions[i].x + deformation, 
@@ -32,8 +43,18 @@ public class RockCreator : MonoBehaviour
             float remappedOffset = math.remap(0f, 1f, -1f, 1f, offset);
             positions[i].x += remappedOffset;
             positions[i].y += remappedOffset;
+            
+            Debug.Log(positions[i]);
         }
-        lr.SetPositions(positions);
-        lr.loop=true;
+        lineRenderer.SetPositions(positions);
+        lineRenderer.loop=true;
+        lineRenderer.useWorldSpace = false;
+
+       
+    }
+
+    private void OnValidate()
+    {
+        GenerateGeometry();
     }
 }
